e7eb7192a416249659b8ad78e51aa5b15dd66ed5
Matt Lim <milttam1@gmail.com>
2024-04-21 15:39:13 +0200

    Added VGG16 Model

--- model/ResNet.py
+++ model/ResNet.py
@@ -3,12 +3,12 @@ import tensorflow as tf
 def ResNet50(img_size: int, color: str, num_classes: int):
     # Load the pre-trained ResNet50 model
     base_model = tf.keras.applications.ResNet50(
-    include_top=False,
-    #weights='imagenet',
-    input_tensor=None,
-    input_shape=(img_size, img_size, 3 if color == 'rgb' else 1),
-    pooling=None,
-    classifier_activation='softmax'
+        include_top=False,
+        weights=None,
+        input_tensor=None,
+        input_shape=(img_size, img_size, 3 if color == 'rgb' else 1),
+        pooling=None,
+        classifier_activation='softmax'
     )
     x = tf.keras.layers.GlobalAveragePooling2D()(base_model.output)
     x = tf.keras.layers.Dense(1024, activation='relu')(x)  
---
+++ model/vgg.py
@@ -0,0 +1,45 @@
+import tensorflow as tf
+from keras.applications import vgg16
+from keras.models import Sequential, Model
+from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Activation
+
+"""
+Summary of Model Archetecture:
+- Base model is VGG16 with no pretrained weights
+- Base model is unfrozen
+- Flatten base model output to 2048 nodes
+- Reduce to 512 nodes
+- Final classification layer (29 classes for ASL)
+
+Total params: 15777501 (60.19 MB) (Use "model.summary()" to see this output)
+"""
+
+def Vgg16(img_size, color, num_classes):
+    # Use a base vgg16 with that is not pretrained, 
+    base_model = vgg16.VGG16(
+        include_top=False,
+        weights = None,
+        input_shape=(img_size, img_size, 3 if color == 'rgb' else 1),
+        pooling=None,
+    )
+
+    # Make sure base model layers are unfrozen, so we can train those parameters
+    for layer in base_model.layers:
+        layer.trainable = True
+
+    # Create a new model by adding layers on top of the base model
+    model = Sequential()
+    model.add(base_model)
+
+    # Flatten current model from 2x2x512 to 2048 nodes
+    model.add(Flatten())
+
+    # Reduce from 2048 -> 512 nodes
+    model.add(Dense(512, activation='relu')) # Can change this reduction accordingly
+
+    # Final classification layer
+    model.add(Dense(num_classes, activation='softmax'))
+
+    return model
+
+# NOTE: Use "model.summary()" to see the model architecture
